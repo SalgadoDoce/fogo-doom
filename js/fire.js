497106b52e3f096e1965d63358d40daf53553d6a
Salgado Doce <salgadodoce832@gmail.com>
2019-09-14 16:40:13 -0300

    withoutColors

--- js/fire.js
+++ js/fire.js
@@ -1,10 +1,13 @@
 const firePixels = []
-const WIDTH = 3
-const HEIGHT = 2
+const WIDTH = 10
+const HEIGHT = 10
 
 const start = () => {
     createFireDataStructure()
+    createFireSource()
     renderFire()
+    calculateFirePropagation()
+    setInterval(calculateFirePropagation, 1000)
 }
 
 const createFireDataStructure = () => {
@@ -15,6 +18,31 @@ const createFireDataStructure = () => {
     }
 }
 
+const updateFireIntensityPerPixel = (currentPixel) => {
+    const belowPixelIndex = currentPixel + WIDTH
+    if(belowPixelIndex >= WIDTH * HEIGHT) {
+        return
+    }
+
+    const decay = Math.floor(Math.random() * 3)
+    const belowPixelFireIntensity = firePixels[belowPixelIndex]
+    const newFireIntensity = 
+    belowPixelFireIntensity - decay >= 0 ? belowPixelFireIntensity - decay : 0
+
+    firePixels[currentPixel] = newFireIntensity
+}
+
+const calculateFirePropagation = () => {
+    for(let column = 0; column < WIDTH; column++) {
+        for(let row = 0; row < HEIGHT; row++) {
+            const pixelIndex = column + (WIDTH * row)
+            updateFireIntensityPerPixel(pixelIndex)
+        }
+    }
+
+    renderFire()
+}
+
 const renderFire = () => {
     let html = `<table cellpadding=0 cellspacing=0>`
     
@@ -23,8 +51,9 @@ const renderFire = () => {
 
         for(let column = 0; column < WIDTH; column++) {
             const pixelIndex = column + (WIDTH * row)
+            const fireIntensity = firePixels[pixelIndex]
 
-            html += `<td>${pixelIndex}</td>`
+            html += `<td><div class="pixel-index">${pixelIndex}</div>${fireIntensity}</td>`
         }
 
         html += `</tr>`
@@ -33,6 +62,15 @@ const renderFire = () => {
     html += `</table>`
 
     document.querySelector("div#fireCanvas").innerHTML = html
+}
+
+const createFireSource = () => {
+    for(let column = 0; column < WIDTH; column++) {
+        const overFlowPixelIndex = WIDTH * HEIGHT
+        const pixelIndex = (overFlowPixelIndex - WIDTH) + column
+
+        firePixels[pixelIndex] = 36
+    }
 }
 
 start()
